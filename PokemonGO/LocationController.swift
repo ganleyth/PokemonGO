b56a52ff011eab50036ca86899d28408532e2faa
ganleyth <tganley3@gmail.com>
2017-10-27 16:06:50 -0400

    Add user current location to the map

---
+++ PokemonGO/LocationController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationController.swift
+//  PokemonGO
+//
+//  Created by Thomas Ganley on 10/27/17.
+//  Copyright © 2017 Thomas Ganley. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationController {
+    
+    static let shared = LocationController()
+    let locationManager = CLLocationManager()
+}
--- PokemonGO/MapViewController.swift
+++ PokemonGO/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
@@ -16,24 +17,12 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        setupMap()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Helper methods
+    func setupMap() {
+        mapView.showsUserLocation = true
     }
-    */
-
 }
