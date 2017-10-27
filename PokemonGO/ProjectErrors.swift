d3e05738e5dee19cc282b699ee5ef039529cdabe
ganleyth <tganley3@gmail.com>
2017-10-26 20:22:44 -0400

    Add constants and errors structs for the project

---
+++ PokemonGO/ProjectErrors.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectErrors.swift
+//  PokemonGO
+//
+//  Created by Thomas Ganley on 10/26/17.
+//  Copyright © 2017 Thomas Ganley. All rights reserved.
+//
+
+import Foundation
+
+struct ProjectErrors {
+    
+}
