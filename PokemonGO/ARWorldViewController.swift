43ddd659028edc4e1cb7e460a5a6af0fd078ddba
ganleyth <tganley3@gmail.com>
2017-10-31 13:00:16 -0400

    Add horizontal transition between the map and AR VCs

--- PokemonGO/ARWorldViewController.swift
+++ PokemonGO/ARWorldViewController.swift
@@ -16,17 +16,9 @@ class ARWorldViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func enterMapButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
 }
